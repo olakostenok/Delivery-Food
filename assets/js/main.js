884586b0ada088f15e3e6774c879e6a37eaf91bc
Olya Kostenok <ola.kostenok@gmail.com>
2020-05-07 12:10:13 +0300

    feat:added elements via js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,9 @@
+// new WOW().init();
+'use strict'
+
 const cartButton = document.querySelector('#cart-button');
 const modal = document.querySelector('.modal');
 const close = document.querySelector('.close');
-
-cartButton.addEventListener('click', toggleModal);
-close.addEventListener('click',toggleModal);
-
-function toggleModal() {
-    modal.classList.toggle('is-open');
-}
-
-// new WOW().init();
-
 const buttonAuth = document.querySelector('.button-auth');
 const modalAuth = document.querySelector('.modal-auth');
 const closeAuth = document.querySelector('.close-auth');
@@ -18,14 +11,23 @@ const logInForm = document.querySelector('#logInForm');
 const logInInput = document.querySelector('#login');
 const userName = document.querySelector('.user-name');
 const buttonOut = document.querySelector('.button-out');
+const cardsRestaurants = document.querySelector('.cards-restaurants');
+const containerPromo = document.querySelector('.container-promo');
+const restaurants = document.querySelector('.restaurants');
+const menu = document.querySelector('.menu');
+const logo = document.querySelector('.logo');
+const cardsMenu = document.querySelector('.cards-menu');
 let login = localStorage.getItem('User');
+
+function toggleModal() {
+    modal.classList.toggle('is-open');
+}
 
 function toggleModalAuth() {
     modalAuth.classList.toggle('is-open');
 }
 
 function authorized() {
-
     function logOut() {
         login = null;
         localStorage.removeItem('User');
@@ -75,3 +77,82 @@ function checkAuth() {
 }
 
 checkAuth();
+
+function cerateCardRestaurant() {
+    const card = `
+    <a class="card card-restaurant">
+		<img src="assets/images/tanuki/preview.jpg" alt="image" class="card-image"/>
+		<div class="card-text">
+			<div class="card-heading">
+				<h3 class="card-title">Тануки</h3>
+				<span class="card-tag tag">60 мин</span>
+			</div>
+			<div class="card-info">
+				<div class="rating">
+					4.5
+				</div>
+				<div class="price">От 1 200 ₽</div>
+				<div class="category">Суши, роллы</div>
+			</div>
+		</div>					
+	</a>
+    `;
+    cardsRestaurants.insertAdjacentHTML('beforeend', card)
+}
+
+cerateCardRestaurant();
+cerateCardRestaurant();
+
+function createCardGood() {
+    const card = document.createElement('div');
+    card.className = 'card';
+    card.insertAdjacentHTML('beforeend', `
+        <img src="assets/images/pizza-plus/pizza-classic.jpg" alt="image" class="card-image"/>
+        <div class="card-text">
+            <div class="card-heading">
+                <h3 class="card-title card-title-reg">Пицца Классика</h3>
+            </div>
+            <div class="card-info">
+                <div class="ingredients">Соус томатный, сыр «Моцарелла», сыр «Пармезан», ветчина, салями, грибы.
+                </div>
+            </div>
+            <div class="card-buttons">
+                <button class="button button-primary button-add-cart">
+                    <span class="button-card-text">В корзину</span>
+                    <span class="button-cart-svg"></span>
+                </button>
+                <strong class="card-price-bold">510 ₽</strong>
+            </div>
+        </div>
+    `);
+
+    cardsMenu.insertAdjacentElement('beforeend', card);
+}
+
+function openGoods(event) {  
+    const target = event.target;
+    const restaurant = target.closest('.card-restaurant');
+    if(restaurant) {
+        cardsMenu.textContent = ``;
+        containerPromo.classList.add('hide');
+        restaurants.classList.add('hide');
+        menu.classList.remove('hide');
+
+        createCardGood();
+        createCardGood();
+    }
+
+    
+
+}
+cartButton.addEventListener('click', toggleModal);
+
+close.addEventListener('click',toggleModal);
+
+cardsRestaurants.addEventListener('click', openGoods);
+
+logo.addEventListener('click', function() {
+    containerPromo.classList.remove('hide');
+    restaurants.classList.remove('hide');
+    menu.classList.add('hide');
+})
